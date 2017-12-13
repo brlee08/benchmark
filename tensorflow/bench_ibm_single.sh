872c9f56d1d88478f8395597270e2669598204a3
brlee08 <biellona@gmail.com>
2017-12-13 14:28:57 +0900

    Update bench_ibm_single.sh
    Added candidates for 'INPUT_MODEL' .

--- tensorflow/bench_ibm_single.sh
+++ tensorflow/bench_ibm_single.sh
@@ -2,7 +2,7 @@
 
 DATA_DIR=/nvme3T/brlee/imagenet/tfrecord
 LOG_DIR=/gpfs/gpfs_gl4_16mb/b7p284za/benchmark/tensorflow/output_single
-EVAL_DIR=/gpfs/gpfs_gl4_16mb/b7p284za/benchmark/tensorflow/eval_single
+#EVAL_DIR=/gpfs/gpfs_gl4_16mb/b7p284za/benchmark/tensorflow/eval_single
 TIMESTAMP=$(date +%m%d%H%M)
 
 #EVALUATION SETTING
@@ -16,13 +16,22 @@ NUM_GPU=4
 
 INPUT_BATCH=128
 INPUT_MODEL="googlenet"
+#INPUT_MODEL="alexnet"
+#INPUT_MODEL="vgg11"
+#INPUT_MODEL="vgg16"
+#INPUT_MODEL="vgg19"
+#INPUT_MODEL="lenet"
+#INPUT_MODEL="overfeat"
+#INPUT_MODEL="trivial"
+#INPUT_MODEL="inception3"
+#INPUT_MODEL="inception4"
+#INPUT_MODEL="resnet50"
+#INPUT_MODEL="resnet101"
+#INPUT_MODEL="resnet152"
 
 NUM_BATCHES=$((${SAMPLE_SIZE} * ${NUM_EPOCHS} / ${INPUT_BATCH} / ${NUM_GPU}))
 #NUM_BATCHES=1000
 VARIABLE_UPDATE=replicated
-
-TRAIN_LOG_DIR="${TRAIN_DIR}/googlenet-10e-128b-4G"
-#TRAIN_LOG_DIR="${TRAIN_LOG}/${INPUT_MODEL}_${NUM_EPOCHS}e_${INPUT_BATCH}b_${NUM_GPU}G"
 
 echo "NUM_BATCHES : ${NUM_BATCHES}"
 source /opt/DL/tensorflow/bin/tensorflow-activate
@@ -37,11 +46,9 @@ train() {
     LOG_FILE="${LOG_DIR}/output_${MODEL}_e${NUM_EPOCHS}_b${BATCH_SIZE}.${TIMESTAMP}.log"
     python tf_cnn_benchmarks/tf_cnn_benchmarks.py \
         --model=${MODEL} --batch_size=${BATCH_SIZE} --num_batches=${NUM_BATCHES} --num_gpus=${NUM_GPU} \
-        --data_name=imagenet --train_dir=${TRAIN_LOG_DIR} --data_dir=${DATA_DIR} --variable_update=${VARIABLE_UPDATE} \
+        --data_name=imagenet --train_dir=${TRAIN_DIR} --data_dir=${DATA_DIR} --variable_update=${VARIABLE_UPDATE} \
 	--summary_verbosity=${SUMMARY_VERBOSITY} --save_summaries_steps=${SUMMARIES_STEPS} \
         2>&1 | tee ${LOG_FILE}
 }
 
 time train ${INPUT_MODEL} ${INPUT_BATCH}
-#time train googlenet ${BATCH}
-#time train resnet101 112
